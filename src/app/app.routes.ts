94465cbc637fb7550911bcdccbb5d0a46582d936
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-07-02 18:30:08 +0200

    setup projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
-import { LoginComponent } from '../pages/login/login.component';
-import { RegisterComponent } from '../pages/register/register.component';
-import { AuthGuard } from '../guard/auth.guard'
-import { ContactComponent } from '../pages/contact/contact.component';
-import { ProfilComponent } from '../pages/profil/profil.component';
-
 
 
 
@@ -14,20 +8,7 @@ export const routes: Routes = [
     {
         path:'', component: HomeComponent
     },
-    {
-        path:'contact', component: ContactComponent, //canActivate: [AuthGuard]
-    },
-           
-    {
-        path:'register', component: RegisterComponent
-    },
-
-    {
-        path:'login', component: LoginComponent 
-    },
-    {
-        path:'profil', component: ProfilComponent, canActivate: [AuthGuard], 
-    },
+   
    
     
 ];
--- src/interface/decoded-token.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "./user";
-
-export interface DecodedToken extends User  {
-    userId: string;
-    exp: number; 
-  }
